6e7e4a9bf30186a837a03ab807696052d07f8df2
Programmer <mnvnm@naver.com>
2020-07-01 11:03:51 +0900

    Dodge 6 29

---
+++ Dodge 2020 6 29/Assets/Dodge/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float Speed = 8;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        float Fire = Input.GetAxis("Fire1");
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * Speed) ;
+
+        if (Fire != 0) Die();
+
+        if (Input.GetKeyDown(KeyCode.Space)) // 키보드에 스페이스가 눌렸을때
+        {
+            Debug.Log("누름");
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) // 키보드에 스페이스가 누르고 뗐을때
+        {
+            Debug.Log("땜");
+        }
+
+
+        //if (Input.GetKey(KeyCode.UpArrow))// 키보드에 스페이스가 눌려져 있을때
+        //{
+        //    Debug.Log("앞");
+        //    //transform.position += Vector3.forward * Speed * Time.deltaTime;
+        //    //rigidbody.AddForce(Vector3.forward * Speed);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    Debug.Log("뒤");
+        //    //transform.position += Vector3.back * Speed * Time.deltaTime;
+        //    //rigidbody.AddForce(Vector3.back * Speed);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    Debug.Log("왼");
+        //    //transform.position += Vector3.left * Speed * Time.deltaTime;
+        //    //rigidbody.AddForce(Vector3.left * Speed);
+        //}
+        //if(Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    Debug.Log("오");
+        //    //transform.position += Vector3.right * Speed * Time.deltaTime;
+        //    //rigidbody.AddForce(Vector3.right * Speed);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.F)) Die();
+    }
+
+    public void  Die()
+    {
+        Debug.Log("Die");
+        gameObject.SetActive(false);
+    }
+}
---
+++ Dodge 2020 6 29/Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : EditorWindow
+{
+    [MenuItem("Winodw/Test")]
+    // Start is called before the first frame update
+    static void Init()
+    {
+        Test window = (Test)EditorWindow.GetWindow(typeof(Test));
+        window.Show();
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        Handles.color = Color.red ;
+        Handles.DrawRectangle(0, new Vector3(160, 200, 0), Quaternion.identity,80 );
+        Handles.DrawSolidDisc(new Vector3(60, 200, 0),Vector3.forward, 50);
+        Handles.color = Color.yellow;
+        Handles.DrawSolidDisc(new Vector3(160, 200, 0),Vector3.forward, 50);
+        Handles.color = Color.green;
+        Handles.DrawSolidDisc(new Vector3(260, 200, 0),Vector3.forward, 50);
+    }
+}
